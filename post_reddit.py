57c4dd8181e2c1cbb0c8fe8d03c18b05a3bd0589
CoD-Segfault <rudy.host@gmail.com>
2021-06-22 08:31:02 -0500

    Add weekend summary to Monday posts

--- post_reddit.py
+++ post_reddit.py
@@ -30,6 +30,21 @@ vaccine_doses = todays_data['vaccine_doses']
 vaccine_average = round(todays_data['vaccine_rolling_average'])
 percent_vaccinated = todays_data['total_population_percent_vaccinated']
 
+# Check if it's Monday and add weekend stats 
+if today.isoweekday() == 1:
+    sunday = today - timedelta(1)
+    sunday_formatted = format_date(sunday)
+    sunday_data = combined_data[sunday_formatted]
+
+    saturday = today - timedelta(2)
+    saturday_formatted = format_date(saturday)
+    saturday_data = combined_data[saturday_formatted]
+
+    weekend_cases = sunday_data['cases'] + saturday_data['cases']
+    weekend_deaths = sunday_data['deaths'] + saturday_data['deaths']
+    weekend_doses = sunday_data['vaccine_doses'] + saturday_data['vaccine_doses']
+
+
 # Generate the title and text based on current data.
 title = f"Unofficial Daily Update for {today_formatted}. {new_cases} New Cases."
 
@@ -37,12 +52,15 @@ selftext = (f"There were {new_cases:,} reported today.  With {tests:,} tests adm
         f"There were {deaths:,} reported deaths.\n\n"
         f"There are {hospitalizations:,} hospitalizations, with {icu_usage:,} in the ICU, and {ventilator_usage:,} ventilators in use.\n\n"
         f"{vaccine_doses:,} vaccine doses were administered yesterday, bringing the 7 day rolling average to {vaccine_average:,}.\n\n"
-        f"{percent_vaccinated}% of the total Illinois population are fully vaccinated.\n\n\n"
-        f"{weekly_reference(combined_data, reference_date=today)}\n\n"
+        f"{percent_vaccinated}% of the total Illinois population are fully vaccinated.\n\n")
+
+if today.isoweekday() == 1:
+    selftext += f"In addition to today's numbers, there have been {weekend_cases:,} new cases, {weekend_deaths:,} deaths, and {weekend_doses:,} vaccine doses administered since Friday's post.\n\n"
+        
+selftext += (f"{weekly_reference(combined_data, reference_date=today)}\n\n"
         f"{week_comparison(combined_data, reference_date=today)}\n\n"
         "This post was automatically generated based on the latest data from the IDPH website.  \n"
         "Source code is available at https://github.com/CoD-Segfault/covid_il_bot")
-
 
 credentials_file = open("credentials.json")
 credentials = json.load(credentials_file)
