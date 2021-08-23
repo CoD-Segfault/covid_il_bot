28bc5f27fa76cfa3d0dc650fe10de53558b94b55
brettgus <brettgus@users.noreply.github.com>
2021-08-23 11:15:10 -0500

    Fixing verbage for Monday/weekend comment reply (#3)
    * Fixing verbage for Monday/weekend comment reply
    
    * Fixing verbage for Monday/weekend comment reply - Cleaner code
    
    Fixing verbage for Monday/weekend comment reply - Cleaner code
    
    * Removed + concatenation

--- format_data.py
+++ format_data.py
@@ -23,12 +23,15 @@ def weekly_reference(combined_data, reference_date = date.today()):
     cases_twentyone_days_ago = combined_data[twentyone_days_ago]['cases']
     cases_twentyeight_days_ago = combined_data[twentyeight_days_ago]['cases']
 
+    ago_prior = "Ago" if (reference_date == date.today()) else "Prior"
+    reference_day_of_week = "Today" if (reference_date == date.today()) else reference_date.strftime('%A')
+
     return (f"Week over week reference:  \n"
-            f"28 Days Ago: {cases_twentyeight_days_ago:,}  \n"
-            f"21 Days Ago: {cases_twentyone_days_ago:,}  \n"
-            f"14 Days Ago: {cases_fourteen_days_ago:,}  \n"
-            f"7 Days Ago: {cases_seven_days_ago:,}  \n"
-            f"Today: {cases_today:,}  \n")
+    f"28 Days {ago_prior}: {cases_twentyeight_days_ago:,}  \n"
+    f"21 Days {ago_prior}: {cases_twentyone_days_ago:,}  \n"
+    f"14 Days {ago_prior}: {cases_fourteen_days_ago:,}  \n" 
+    f"7 Days {ago_prior}: {cases_seven_days_ago:,}  \n"
+    f"{reference_day_of_week}: {cases_today:,}  \n")
 
 def weekly_average(combined_data, metric, reference_date = date.today()):
     total = 0
