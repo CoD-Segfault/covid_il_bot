0f101553af19c96d615bcbd6f2978867c0e159b6
CoD-Segfault <rudy.host@gmail.com>
2021-08-31 13:56:16 -0500

    Fix condition where only some data is missing
    Ran into situations where one data source was missing data, but the check for data did not account for it.  Should catch cases where we grab the data as it's being updated.

--- get_data.py
+++ get_data.py
@@ -105,10 +105,10 @@ def get_idph_data():
     """
 
     # Check to make sure that the data for today is available, otherwise try again in 30 seconds.
-    if today_formatted not in combined_data:
-        print("Data not abailable yet, pausing 30 seconds.")
-        time.sleep(30)
-        combined_data = get_idph_data()
+    #if today_formatted not in combined_data:
+    #    print("Data not available yet, pausing 30 seconds.")
+    #    time.sleep(30)
+    #    combined_data = get_idph_data()
 
     # Ingest CDC data
     for day in cdc_vaccine_data.json():
@@ -144,6 +144,23 @@ def get_idph_data():
         combined_data[normalized_date]['fully_vaccinated_percent_12plus'] = fully_vaccinated_12plus
         combined_data[normalized_date]['fully_vaccinated_percent_18plus'] = fully_vaccinated_18plus
         combined_data[normalized_date]['fully_vaccinated_percent_65plus'] = fully_vaccinated_65plus
+        
+        
+    # Check to make sure each data source had data.  Saw conditions where we retrieve data as it's being updated and crash.
+    if "cases" not in combined_data[today_formatted]:
+        print("IDPH case/test data not available yet, pausing 30 seconds.")
+        time.sleep(30)
+        combined_data = get_idph_data()
+        
+    if "covid_vent" not in combined_data[today_formatted]:
+        print("IDPH hospitalization data not available yet, pausing 30 seconds.")
+        time.sleep(30)
+        combined_data = get_idph_data()
+        
+    if "vaccines_administered_total" not in combined_data[today_formatted]:
+        print("CDC vaccine data not available yet, pausing 30 seconds.")
+        time.sleep(30)
+        combined_data = get_idph_data()
 
     return combined_data
     
